95f777a4c30b7ef79d4e1d4819ae9d7ee06b5e0f
imdud <imdud@121.165.95.191>
2021-12-18 14:04:03 +0900

    second Commit

--- DoItJava/src/chaptor2/Variable1.java
+++ DoItJava/src/chaptor2/Variable1.java
@@ -3,8 +3,9 @@ package chaptor2;
 public class Variable1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int level; //정수형 변수 level을 선언
+		level = 10; //level 변수에 값 10을 대입
+		System.out.println(level); //level 값 출력
 	}
 
 }
---
+++ DoItJava/src/chaptor2/Variable1_review.java
@@ -0,0 +1,13 @@
+package chaptor2;
+
+public class Variable1_review {
+
+	public static void main(String[] args) {
+		//level 대신 나이 대입
+		int age;
+		age = 27;
+		System.out.println(age);
+
+	}
+
+}
